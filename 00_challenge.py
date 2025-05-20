264a7f42f1172a821371ac48acae5dcf3f4f4bc9
keniernn <keniernn@gmail.com>
2025-05-20 12:16:20 +0200

    SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ 00_challenge.py
@@ -0,0 +1,40 @@
+
+
+#SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+#----------------------
+#https://www.python.org/
+#----------------------
+"""
+(
+en varia líneas
+.
+.
+.
+.
+)
+"""
+#----------------------
+#En una linea
+#----------------------
+index = "Learing Python"
+#----------------------
+strs = "python"
+ints = 1
+floats = 0.1
+bools = False | True
+lists = [1,2,3,4,5,6,7,8,9,9]
+sets = {1,2,3,4,5,6,7,8,9,9}
+tuples = (1,2,3,4,5,6,7,8,9,9)
+dicts = {"value_one":1,"value_two":3,"value_three":3,"value_four":4}
+
+
+print(f"¡Hola, {strs}!")
+
+
+
+
+
+
+
+
