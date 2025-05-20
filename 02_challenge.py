16402d027cd3d1c8dd19f4fc6592cf247de5ec25
keniernn <keniernn@gmail.com>
2025-05-20 14:02:45 +0200

    FUNCIONES Y ALCANCE

--- 02_challenge.py
+++ 02_challenge.py
@@ -22,3 +22,47 @@ DIFICULTAD EXTRA (opcional):
  * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
  * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
 """
+
+def sin_parametros():
+    pass
+
+def con_varios_parametros(a,b,c):
+    def dentro_funcion():
+        return print(f"inside of the function.")
+    dentro_funcion()
+    return a + b + c
+
+#variable local
+def funcion_con_variable_local(value_one, text2):
+    value_one = "improving of the python skills." #local
+    value_two = text2 
+    return f"{value_one}\n{value_two}"
+
+##global
+value_one = "python"
+value_two = "global VARIABLE"
+
+print(sin_parametros())
+print(con_varios_parametros(1,2,3))
+print(funcion_con_variable_local(value_one,value_two))
+
+
+# * DIFICULTAD EXTRA (opcional):
+
+def index(txt1,txt2):
+    space = " "
+    count = 0
+    for numero in range(1,101):
+        if numero % 3 == 0 and numero % 5 == 0:
+            print(txt1 + space + txt2)
+        elif numero % 3 == 0:
+            print(txt1)
+        elif numero % 5 == 0:
+            print(txt2)
+        else:
+            count +=1
+    return f"El número de veces que se ha impreso el número {count}"
+
+a,b = "learn", "python"
+print(index(a,b))
+
